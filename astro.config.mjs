5db8f69a609278c2854a903d00e202f9d90a907b
mahdim43 <hooshmandm43@gmail.com>
2026-07-18 09:27:36 +0330

    Initial commit: Astro portfolio site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mahdim43.github.io",
+  base: "/surendar-portfolio",
+});
